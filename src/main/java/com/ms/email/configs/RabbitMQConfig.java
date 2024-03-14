67158e301c4a23eb0ed34f7556eb61a0240e28a7
Felipe Fonseca <160867462+felipefonnsec@users.noreply.github.com>
2024-03-14 14:31:43 -0300

    first commit

---
+++ src/main/java/com/ms/email/configs/RabbitMQConfig.java
@@ -0,0 +1,30 @@
+package com.ms.email.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    //instanciando o nome da fila
+    @Value("${broker.queue.email.name}")
+    private String queue;
+
+    //criando a fila
+    @Bean
+    public Queue queue(){
+        return new Queue(queue, true);
+    }
+
+    //cria a config de conversao
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+
+}
